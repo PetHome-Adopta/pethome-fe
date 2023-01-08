721ad1b023a19c9272ede68d17b7e3a5503d276a
Xavi Ibáñez Martínez <241xavi@gmail.com>
2023-01-08 19:15:27 +0100

    tutorail stuff

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,41 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-    name: "DefaultLayout"
+    name: "DefaultLayout",
+    setup() {
+        const layout = 'default';
+    },
+    created() {
+        console.log("test created");
+    },
+    mounted() {
+        console.log("test mounted");
+    },
+    updated() {
+        console.log("test updated");
+    },
+    methods: {
+        fetchData(test) {
+            console.log(test);
+        },
+        fetchData2(e) {
+            console.log("test fetch data 2");
+            console.log(e);
+        }
+    },
+    computed: {
+    },
+    data() {
+        return {}
+    }
 })
+
 </script>
 <template>
-    
+    <div>
+        <h1>Test</h1>
+        <slot/>    
+        <button @click="fetchData('test fetch data')">Button 1</button>
+        <button @click="fetchData2">Button 2</button>
+    </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,29 @@
-<template lang="pug">
-  tutorial
-</template>
-
 <script>
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  setup() {
+    const layout = 'default';
+  },
+  created() {
+    console.log("test created");
+  },
+  mounted() {
+  },
+  updated() {
+  },
+  methods: {
+  },
+  computed: {
+  },
+  data() {
+  },
 })
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
