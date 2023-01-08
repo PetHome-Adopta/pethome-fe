a64e774a70258efb8fee49e0b6e37717d02e0af4
Xavi Ibáñez Martínez <241xavi@gmail.com>
2023-01-08 20:46:24 +0100

    update layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,35 +3,13 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'DefaultLayout',
-  setup() {
-    const layout = 'default'
-  },
-  created() {
-    console.log('test created')
-  },
-  mounted() {
-    console.log('test mounted')
-  },
-  updated() {
-    console.log('test updated')
-  },
-  methods: {
-    fetchData(test) {
-      console.log(test)
-    },
-    fetchData2(e) {
-      console.log('test fetch data 2')
-      console.log(e)
-    },
-  },
-  computed: {},
-  data() {
-    return {}
-  },
 })
+
 </script>
 <template>
   <div>
+    <Header/>
     <Nuxt />
+    <Footer/>
   </div>
 </template>
