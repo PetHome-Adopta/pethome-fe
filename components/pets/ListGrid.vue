384a875ac171a49fb0ee2e09cd7e296d4bbe9d65
Xavi Ibáñez Martínez <241xavi@gmail.com>
2023-01-11 23:23:56 +0100

    add pets data

--- components/pets/ListGrid.vue
+++ components/pets/ListGrid.vue
@@ -5,9 +5,17 @@ import { Backends } from '~/@core/backendList';
 export default defineComponent({
   name: 'PetsListGrid',
   async created() {
-    await this.$store.dispatch("getItems", {backend: Backends.API, itemType: "/api/v1/pets", requestConfig: {}, customName: "pets", page: 1})
+    this.pets = await this.$store.dispatch("getItems", {backend: Backends.API, itemType: "api/v1/pets", requestConfig: {}, customName: "pets", page: 1})
+    //TODO: como mando test al template?
+    console.log('pets: ', this.pets.data)
+    //TODO: que es esto -> this.$store.getters?
     console.log(this.$store.getters)
   },
+  data() {
+    return {
+      pets: []
+    }
+  }
 })
 
 </script>
@@ -18,5 +26,11 @@ export default defineComponent({
         <b-col>2 of 3</b-col>
         <b-col>3 of 3</b-col>
     </b-row>
+  </div>
+  <div>
+    <h1>Nuxt test</h1>
+      <ul>
+        <li v-for="pet of pets">{{ pet.name }}</li>
+      </ul>
   </div>
 </template>
