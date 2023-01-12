87f388857208392535814be1a9133046433ce3e1
Xavi Ibáñez Martínez <241xavi@gmail.com>
2023-01-12 23:20:43 +0100

    I tried everything and still fails

--- components/pets/ListGrid.vue
+++ components/pets/ListGrid.vue
@@ -1,22 +1,48 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { Backends } from '~/@core/backendList';
 
 export default defineComponent({
   name: 'PetsListGrid',
-  async created() {
+  data() {
+    return {
+      pets: ref({data:[]}),
+    }
+  },
+  async mounted() {
     this.pets = await this.$store.dispatch("getItems", {backend: Backends.API, itemType: "api/v1/pets", requestConfig: {}, customName: "pets", page: 1})
-    console.log('pets: ', this.pets.data)
+    console.log('pets: ', this.pets?.data)
+  },
+  computed: {
+    petsList() {
+      return this.pets?.data;
+    }
   }
 })
 
 </script>
 <template>
   <div>
-    <b-row>
-        <b-col>1 of 3</b-col>
-        <b-col>2 of 3</b-col>
-        <b-col>3 of 3</b-col>
-    </b-row>
+    <div>
+      <pre>{{petsList}}</pre>  
+    </div>
+    <div>
+      <b-row>
+          <b-col>1 of 3</b-col>
+          <b-col>2 of 3</b-col>
+          <b-col>3 of 3</b-col>
+      </b-row>
+    </div>
+    <div v-if="petsList">
+      <ul>
+        <li v-for="pet in petsList" :key="pet.key">
+          {{ pet.name }}
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      Loading...
+    </div>
+    
   </div>
 </template>
