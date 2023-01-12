ec8c835b065b6a0570ecf862e56d2b06fa1b6758
Xavi Ibáñez Martínez <241xavi@gmail.com>
2023-01-12 22:41:46 +0100

    remove useles code

--- components/pets/ListGrid.vue
+++ components/pets/ListGrid.vue
@@ -6,15 +6,7 @@ export default defineComponent({
   name: 'PetsListGrid',
   async created() {
     this.pets = await this.$store.dispatch("getItems", {backend: Backends.API, itemType: "api/v1/pets", requestConfig: {}, customName: "pets", page: 1})
-    //TODO: como mando test al template?
     console.log('pets: ', this.pets.data)
-    //TODO: que es esto -> this.$store.getters?
-    console.log(this.$store.getters)
-  },
-  data() {
-    return {
-      pets: []
-    }
   }
 })
 
@@ -26,11 +18,5 @@ export default defineComponent({
         <b-col>2 of 3</b-col>
         <b-col>3 of 3</b-col>
     </b-row>
-  </div>
-  <div>
-    <h1>Nuxt test</h1>
-      <ul>
-        <li v-for="pet of pets">{{ pet.name }}</li>
-      </ul>
   </div>
 </template>
