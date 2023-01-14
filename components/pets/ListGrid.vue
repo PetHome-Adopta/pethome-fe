d1bd183f985ab36fcd39597f87841a583f5b6b16
Xavi Ibáñez Martínez <241xavi@gmail.com>
2023-01-15 00:59:25 +0100

    finish listing pets

--- components/pets/ListGrid.vue
+++ components/pets/ListGrid.vue
@@ -5,18 +5,29 @@ import { Backends } from '~/@core/backendList';
 export default defineComponent({
   name: 'PetsListGrid',
   async created() {
-    await this.$store.dispatch("getItems", {backend: Backends.API, itemType: "/api/v1/pets", requestConfig: {}, customName: "pets", page: 1})
-    console.log(this.$store.getters)
+    await this.$store.dispatch("getItems", {backend: Backends.API, itemType: "api/v1/pets", requestConfig: {}, customName: "pets", page: 1})
+    console.log(JSON.stringify(this.$store.getters.items?.pets?.items[0]))
+    console.log("Count:" + JSON.stringify(this.$store.getters.items?.pets?.items[1]))
   },
+  computed: {
+    pets() {
+      return this.$store.getters.items?.pets?.items[0];
+    }
+  }
 })
 
 </script>
 <template>
   <div>
     <b-row>
-        <b-col>1 of 3</b-col>
-        <b-col>2 of 3</b-col>
-        <b-col>3 of 3</b-col>
+      <b-col>Name:</b-col>
+      <b-col>Description:</b-col>
     </b-row>
+      <div v-for="pet of pets" :key="pet.key">
+        <b-row>
+          <b-col>{{ pet.name }}</b-col>
+          <b-col>{{ pet.description }}</b-col>
+        </b-row>
+      </div>
   </div>
 </template>
