839405f49a019b252b5f27c389ea59cdf518bdfc
Xavi Ibáñez Martínez <241xavi@gmail.com>
2023-01-15 22:14:37 +0100

    add pets item

---
+++ components/pets/Item.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PetItem',
+  props: {
+    pet: {
+      type: Object,
+      required: true,
+    },
+  },
+})
+</script>
+<template>
+  <div class="pet-card rounded mt-3 ml-4 p-0">
+    <div class="pet-image rounded">
+      <img src="default.jpg" class="rounded" />
+      <div
+        v-if="pet.urgentAdoption"
+        class="text-white p-2 text-center urgent"
+      >
+        <p>URGENTE!</p>
+      </div>
+    </div>
+
+    <div>
+      <div class="mt-3 pl-1">
+        <h4>{{ pet.name }}</h4>
+        <span v-if="pet.shelter">{{ pet.shelter.address }}</span>
+        <span v-if="pet.gender && pet.breed"
+          >{{ pet.gender }} | {{ pet.breed }}</span
+        >
+      </div>
+    </div>
+  </div>
+</template>
--- components/pets/ListGrid.vue
+++ components/pets/ListGrid.vue
@@ -1,33 +1,36 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { Backends } from '~/@core/backendList';
+import { Backends } from '~/@core/backendList'
 
 export default defineComponent({
   name: 'PetsListGrid',
   async created() {
-    await this.$store.dispatch("getItems", {backend: Backends.API, itemType: "api/v1/pets", requestConfig: {}, customName: "pets", page: 1})
-    console.log(JSON.stringify(this.$store.getters.items?.pets?.items[0]))
-    console.log("Count:" + JSON.stringify(this.$store.getters.items?.pets?.items[1]))
+    await this.fetchData()
+  },
+  methods: {
+    async fetchData() {
+      await this.$store.dispatch('getItems', {
+        backend: Backends.API,
+        itemType: 'api/v1/pets',
+        requestConfig: {},
+        customName: 'pets',
+        page: 1,
+      })
+    },
   },
   computed: {
     pets() {
-      return this.$store.getters.items?.pets?.items[0];
-    }
-  }
+      return this.$store.getters.items?.pets?.items
+    },
+  },
 })
-
 </script>
 <template>
-  <div>
+  <div class="p-5">
     <b-row>
-      <b-col>Name:</b-col>
-      <b-col>Description:</b-col>
+      <b-col cols="3" v-for="pet of pets" :key="pet.key">
+        <PetsItem :pet="pet" />
+      </b-col>
     </b-row>
-      <div v-for="pet of pets" :key="pet.key">
-        <b-row>
-          <b-col>{{ pet.name }}</b-col>
-          <b-col>{{ pet.description }}</b-col>
-        </b-row>
-      </div>
   </div>
 </template>
