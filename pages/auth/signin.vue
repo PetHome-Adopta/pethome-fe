8c4fcea455c7ba2526961a2eb7af3b7c5016d273
Pantaleón Ferrer <61870005+Zettsuen@users.noreply.github.com>
2023-01-22 12:56:20 +0100

    login not finished

--- pages/auth/signin.vue
+++ pages/auth/signin.vue
@@ -3,8 +3,38 @@
 </template>
 
 <script>
+import { Backends } from '~/@core/backendList'
 export default {
+    data(){
+        return {
+            user: "",
+            password: ""
+        }
+    },
+    methods: {
+        async login(){
 
+            try {
+            await this.$store.dispatch("createItem", {
+                backend: Backends.API,
+                itemType: "login",
+                customName: "token",
+                requestConfig: {
+                    user: this.user,
+                    password: this.password
+                }
+            });
+
+            const token = this.$store.getters.items.token;
+            
+
+            }catch(e){
+
+            }
+
+
+        }
+    }
 }
 </script>
 
