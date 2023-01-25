a22c89b7f4ab20d3d3e68171d3777e91442a7734
Xavi Ibáñez Martínez <241xavi@gmail.com>
2023-01-25 23:48:21 +0100

    adding detailed pet div

--- pages/pet/_key.vue
+++ pages/pet/_key.vue
@@ -33,18 +33,47 @@ export default {
 
 <template>
     <div>
-        <b-container class="bv-example-row bv-example-row-flex-cols">
-            <b-row align-v="start">
-                <b-col>{{ pet?.name }} , {{ pet?.age }}</b-col>
-                <b-col/>
-                <b-col><img src="default.jpg" class="rounded" /></b-col>
-            </b-row>
-            <b-row align-v="center">
-                <b-col> Location </b-col>
-            </b-row>
-            <b-row align-v="end">
-                <b-col>{{ pet?.description }}</b-col>
-            </b-row>
-        </b-container>
+        <div class="container-fluid tm-container-content tm-mt-60">
+            <div class="row mb-4">
+                <h2 class="col-12 tm-text-primary">{{ pet?.name }} , {{ pet?.age }}</h2>
+            </div>
+            <div class="row tm-mb-90">            
+                <div class="col-xl-8 col-lg-7 col-md-6 col-sm-12">
+                    <img src="/default.jpg" alt="Image" class="img-fluid">
+                </div>
+                <div class="col-xl-4 col-lg-5 col-md-6 col-sm-12">
+                    <div class="tm-bg-gray tm-video-details">
+                        <p class="mb-4">
+                            Please support us by making <a href="https://paypal.me/templatemo" target="_parent" rel="sponsored">a PayPal donation</a>. Nam ex nibh, efficitur eget libero ut, placerat aliquet justo. Cras nec varius leo.
+                        </p>
+                        <div class="text-center mb-5">
+                            <a href="#" class="btn btn-primary tm-btn-big">Download</a>
+                        </div>                    
+                        <div class="mb-4 d-flex flex-wrap">
+                            <div class="mr-4 mb-2">
+                                <span class="tm-text-gray-dark">Location: </span><span class="tm-text-primary">{{ pet?.shelterKey }}</span>
+                            </div>
+                            <div class="mr-4 mb-2">
+                                <span class="tm-text-gray-dark">Description: </span><span class="tm-text-primary">{{ pet?.description }}</span>
+                            </div>
+                        </div>
+                        <div class="mb-4">
+                            <h3 class="tm-text-gray-dark mb-3">License</h3>
+                            <p>Free for both personal and commercial use. No need to pay anything. No need to make any attribution.</p>
+                        </div>
+                        <div>
+                            <h3 class="tm-text-gray-dark mb-3">Tags</h3>
+                            <a href="#" class="tm-text-primary mr-4 mb-2 d-inline-block">Cloud</a>
+                            <a href="#" class="tm-text-primary mr-4 mb-2 d-inline-block">Bluesky</a>
+                            <a href="#" class="tm-text-primary mr-4 mb-2 d-inline-block">Nature</a>
+                            <a href="#" class="tm-text-primary mr-4 mb-2 d-inline-block">Background</a>
+                            <a href="#" class="tm-text-primary mr-4 mb-2 d-inline-block">Timelapse</a>
+                            <a href="#" class="tm-text-primary mr-4 mb-2 d-inline-block">Night</a>
+                            <a href="#" class="tm-text-primary mr-4 mb-2 d-inline-block">Real Estate</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
