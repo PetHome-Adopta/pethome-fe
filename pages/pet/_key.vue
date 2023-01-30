3fb5b06ad1b37691ddaada0bb02bdc6e053e9b84
Xavi Ibáñez Martínez <241xavi@gmail.com>
2023-01-30 23:56:56 +0100

    get shelter data

--- pages/pet/_key.vue
+++ pages/pet/_key.vue
@@ -3,25 +3,43 @@ import { Backends } from '~/@core/backendList'
 
 export default {
     async created(){
-        if(this.$store.getters.item("pets", this.key)) {
-            return;
+        if(!this.$store.getters.item("pets", this.key)) {
+            await this.fetchDataPet();
         }
-        await this.fetchData();
+        console.log("Pet: ", this.$store.getters.item("pets"));
+        console.log("Shelter key: ", this.$store.getters.item("pets")?.shelterKey);
+        console.log("after Shelter: ", this.$store.getters.item("shelters"));
+        await this.fetchDataShelter();
+        console.log("before Shelter: ", this.$store.getters.item("shelters"));
     },
     methods: {
-        async fetchData(){
+        async fetchDataPet(){
             await this.$store.dispatch('getItems', {
                 backend: Backends.API,
                 itemType: 'api/v1/pets',
                 requestConfig: { key: this.key },
                 customName: 'pets',
                 page: 1,
-            })
+            });
+        },
+        async fetchDataShelter(){
+            console.log("fetch shelters");
+            console.log("this pet: ", this.pet);
+            await this.$store.dispatch('getItems', {
+                backend: Backends.API,
+                itemType: 'api/v1/shelters',
+                requestConfig: { key: this.pet.shelterKey },
+                customName: 'shelters',
+                page: 1,
+            });
         }
     },
     computed: {
         pet(){
             return this.$store.getters.item("pets", this.key);
+        },
+        shelter(){
+            return this.$store.getters.item("shelters", this.pet?.shelterKey);
         },
         key() {
             return this.$route.params.key;
@@ -36,6 +54,9 @@ export default {
         <div class="container-fluid tm-container-content tm-mt-60">
             <div class="row mb-4">
                 <h2 class="col-12 tm-text-primary">{{ pet?.name }} , {{ pet?.age }}</h2>
+            </div>
+            <div class="row mb-4">
+                <h2 class="col-12 tm-text-primary"> {{ shelter?.email }} </h2>
             </div>
             <div class="row tm-mb-90">            
                 <div class="col-xl-8 col-lg-7 col-md-6 col-sm-12">
