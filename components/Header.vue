bbd63d42b4623be39692bda57477cae1464626f6
Pantaleón Ferrer <pantaferrer@hotmail.com>
2023-02-06 18:56:28 +0100

    Modify menu order

--- components/Header.vue
+++ components/Header.vue
@@ -14,9 +14,9 @@ export default defineComponent({
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
         <b-nav-item :to="{name: 'index'}">Inicio</b-nav-item>
-        <b-nav-item :to="{name: 'about'}">Sobre nosotros</b-nav-item>
         <b-nav-item :to="{name: 'pets'}">Animales</b-nav-item>
         <b-nav-item :to="{name: 'shelters'}">Protectoras</b-nav-item>
+        <b-nav-item :to="{name: 'about'}">Sobre nosotros</b-nav-item>
       </b-navbar-nav>
       <b-navbar-nav class="ml-auto">
         <b-nav-item :to="{name: 'auth-signin'}"><b-button class="primary">Entrar</b-button></b-nav-item>
